c8bef899377d403c41bbe1be32dd17c0da8d0e4c
VanemKrAu <119379128+VanemKrAu@users.noreply.github.com>
2026-06-02 19:19:06 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,14 @@
-// sw.js - 标准的 PWA Service Worker
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] 安装成功');
-    self.skipWaiting(); // 强制立即接管控制权
+    self.skipWaiting(); 
 });
 
 self.addEventListener('activate', (e) => {
     console.log('[Service Worker] 激活成功');
-    e.waitUntil(clients.claim()); // 立即控制所有打开的页面
+    e.waitUntil(clients.claim()); 
 });
 
 self.addEventListener('fetch', (e) => {
-    // 拦截网络请求并直接放行。加上这一句，浏览器才会真正认为你具备离线能力。
     e.respondWith(
         fetch(e.request).catch(() => {
             console.log('[Service Worker] 离线请求失败');
